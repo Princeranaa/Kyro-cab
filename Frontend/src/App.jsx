f29e89200e3f6c4c866e58e75e4b832090a272f6
Prince Rana <ranaprince45may@gmail.com>
2025-11-22 19:02:56 +0530

    created UI on frontend side.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import UserLogin from "./pages/UserLogin"
+import UserSignup from "./pages/UserSignup"
+import CaptainSignup from "./pages/CaptainSignup"
+import CaptainLogin from "./pages/CaptainLogin"
+import Home from "./pages/Home"
+
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import kyroImg from '../assets/image3.png';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+function UserLogin() {
+
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const handleChange = (e)=>{
+     setEmail(e.target.value);
+         setPassword(e.target.value)
+    }
+
+
+
+
+    return (
+        <div className='p-8 h-screen flex flex-col justify-between'>
+            <div>
+                <img className='w-16 mb-10' src={kyroImg} alt="" />
+                <form action="">
+                    <h3 className='text-lg font-semibold mb-2'>What's your email</h3>
+                    <input className='bg-[#eeeeee] px-4 py-2 w-full border rounded text-lg placeholder:text-base'required type="email" name="email" placeholder='emai@example.com' value={email} onChange={handleChange} />
+                    <h3 className='text-lg  mb-2 font-semibold'>Enter Password</h3>
+                    <input className='bg-[#eeeeee]  border px-4  py-2  mb-7 w-full rounded  text-lg placeholder:text-base' required type="password" name="password" placeholder='password' value={password} onChange={handleChange} />
+                    <button className='bg-[#111] text-white mb-4 font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base'>Login</button>
+                </form>
+                <div className='flex gap-2 items-center justify-center'>
+                    <p className='text-center'>New here?</p> <Link to={"/signup"} className='text-blue-600'>Create new account</Link>
+                </div>
+            </div>
+
+            <div>
+                <button className='bg-[#10b461] text-white mb-8 font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base'>Sign in as Captain</button>
+            </div>
+        </div>
+    )
+}
+
+export default UserLogin
